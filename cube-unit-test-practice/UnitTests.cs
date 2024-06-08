4e966714909448f8369869b2295ba94a13bb5657
james.hsueh <CodeMachine0121@gmail.com>
2024-06-09 02:01:06 +0800

    feat: question 2

--- cube-unit-test-practice/UnitTests.cs
+++ cube-unit-test-practice/UnitTests.cs
@@ -28,9 +28,30 @@ public class UnitTests
 
             return double.Parse(x, CultureInfo.InvariantCulture) * 0.33;
         }).OrderBy(x => x).ToList();
-        
-        orderedAmountList[0].Should().Be(-0.005 * 0.33);
 
+        orderedAmountList[0].Should().Be(-0.005 * 0.33);
     }
 
+    [Test]
+    public void should_hide_credit_card_number()
+    {
+        var creditCardNumber = "0123456789012345";
+        var hiddenCreditCardNumberCharArray = creditCardNumber.ToCharArray()
+            .Select((x, i) => i < creditCardNumber.Length - 4
+                ? '*'
+                : x).ToArray();
+        
+        var hiddenCreditCardNumberString = new string(hiddenCreditCardNumberCharArray);
+        var length = hiddenCreditCardNumberString.Length;
+
+        var result = new string("");
+        for (var i = 0; i < length; i++)
+        {
+            result += i % 4 == 0 && i != 0
+                ? "-" + hiddenCreditCardNumberString[i]
+                : hiddenCreditCardNumberString[i];
+        }
+
+        result.Should().Be("****-****-****-2345");
+    }
 }
