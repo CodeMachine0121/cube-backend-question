8f8bfbd2a744a72acb77955b256c6dd00d3a899a
james.hsueh <CodeMachine0121@gmail.com>
2024-06-09 01:35:32 +0800

    feat: question 1

--- cube-unit-test-practice/UnitTests.cs
+++ cube-unit-test-practice/UnitTests.cs
@@ -1,3 +1,5 @@
+using System.Globalization;
+using FluentAssertions;
 using NUnit.Framework;
 
 namespace cube_unit_test_practice;
@@ -5,5 +7,30 @@ namespace cube_unit_test_practice;
 [TestFixture]
 public class UnitTests
 {
-    
+    [Test]
+    public void should_be_ordered()
+    {
+        var amountList = new List<string>()
+        {
+            "1.2",
+            "1.4",
+            "0.2",
+            "-",
+            "-0.005"
+        };
+
+        var orderedAmountList = amountList.Select(x =>
+        {
+            if (x == "-")
+            {
+                return 0;
+            }
+
+            return double.Parse(x, CultureInfo.InvariantCulture) * 0.33;
+        }).OrderBy(x => x).ToList();
+        
+        orderedAmountList[0].Should().Be(-0.005 * 0.33);
+
+    }
+
 }
